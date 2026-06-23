65f1d2b278e84bc3c22f743c3dd25f03fc4a63ae
ayushptl2801 <ayushptl2801@gmail.com>
2026-06-23 20:24:26 +0530

    Add files via upload

---
+++ DAY_23/Q90.c
@@ -0,0 +1,22 @@
+/*Write a program to Find first repeating character*/
+
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int seen[256] = {0};
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (seen[(unsigned char)str[i]]) {
+            printf("First repeating character: %c\n", str[i]);
+            return 0;
+        }
+        seen[(unsigned char)str[i]] = 1;
+    }
+
+    printf("No repeating character found.\n");
+    return 0;
+}
