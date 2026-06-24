60fc2c9806c680e3844dbe74af021e3ac517cb7d
ayushptl2801 <ayushptl2801@gmail.com>
2026-06-24 16:13:03 +0530

    Add files via upload

---
+++ DAY_24/Q94.c
@@ -0,0 +1,32 @@
+/*Write a program to Compress a string*/
+
+#include <stdio.h>
+#include <string.h>
+
+void compressString(char str[]) {
+    int count = 1;
+    int len = strlen(str);
+
+    for (int i = 0; i < len; i++) {
+        count = 1;
+
+        while (i < len - 1 && str[i] == str[i + 1]) {
+            count++;
+            i++;
+        }
+
+        printf("%c%d", str[i], count);
+    }
+}
+
+int main() {
+    char str[100];
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    printf("Compressed string: ");
+    compressString(str);
+
+    return 0;
+}
