f695b5407be0e48d4b9d1422050bf43c568a8748
ayushptl2801 <ayushptl2801@gmail.com>
2026-06-25 21:41:30 +0530

    Add files via upload

---
+++ DAY_25/Q98.c
@@ -0,0 +1,29 @@
+/*Write a program to Find common characters in strings*/
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str1[100], str2[100];
+    int found[256] = {0};
+
+    printf("Enter first string: ");
+    scanf("%s", str1);
+
+    printf("Enter second string: ");
+    scanf("%s", str2);
+
+    printf("Common characters: ");
+
+    for (int i = 0; str1[i] != '\0'; i++) {
+        for (int j = 0; str2[j] != '\0'; j++) {
+            if (str1[i] == str2[j] && !found[(unsigned char)str1[i]]) {
+                printf("%c ", str1[i]);
+                found[(unsigned char)str1[i]] = 1;
+                break;
+            }
+        }
+    }
+
+    return 0;
+}
